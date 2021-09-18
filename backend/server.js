5fb5c473e60a8ba51d06241827c92cb1a88f26c5
AC0727 <adele.chen@ashbury.ca>
2021-09-18 14:26:07 -0400

    created index.js in backend

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,6 @@
-const express = require('express');
-const cors = require('cors');
-const mongoose = require('mongoose');
+import express from "express"
+import cors from "cors"
 
-require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -10,10 +8,10 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const uri = process.env.ATLAS_URI;
-mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true }
-);
+app.use("*", (req, res) => res.status(404).json({ error: "not found"}))
 
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
 });
+
+export default app
