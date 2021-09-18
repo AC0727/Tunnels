f3eb59d3b80ea6a97068c49b3266dcb8c5d3fceb
AC0727 <adele.chen@ashbury.ca>
2021-09-18 16:08:59 -0400

    connected database to backend using index.js

--- backend/index.js
+++ backend/index.js
@@ -4,3 +4,22 @@ import dotenv from "dotenv"
 
 dotenv.config()
 const MongoClient = mongodb.MongoClient
+
+const port = process.env.PORT || 8000
+
+MongoClient.connect(
+    process.env.TUNNELS_URI,
+    {
+        poolSize: 50,
+        wtimeout: 2500,
+        useNewUrlParse: true }
+)
+    .catch(err => {
+        console.error(err.stack)
+        process.exit(1)
+    })
+    .then(async client => {
+        app.listen(port, () => {
+            console.log(`listening on port ${port}`)
+        })
+    })
